78ca98b3bb4faadd9401a8752bf59d09ce14d47b
Strating, Michael H <michael.strating@dematic.com>
2020-01-16 14:13:04 -0500

    Fixed post location and file data saving

--- handlers/create.go
+++ handlers/create.go
@@ -18,6 +18,8 @@ func Create(ctx *fasthttp.RequestCtx) {
 	}
 }
 
+// PostData determines what dataset is being posted and dispatches
+// the byte data to the correct handler
 func PostData(dataset string, byteData []byte) interface{} {
 	if dataset == util.MONSTER {
 
@@ -28,24 +30,28 @@ func PostData(dataset string, byteData []byte) interface{} {
 	return byteData
 }
 
+// WriteJSONData writes the byte data to the specified dataset
 func WriteJSONData(dataset string, byteData []byte) {
-	ioutil.WriteFile(dataset+".json", byteData, 0644)
+	ioutil.WriteFile(path.Join(util.DIRNAME, "data", dataset+".json"), byteData, 0644)
 }
 
+// CreateRelational creates a relational data entry to be written to a file
 func CreateRelational(dataset string, lookupDataset string, byteData []byte) RelationalRecord {
 	var rawRelationalRecord RawRelationalRecord
 	json.Unmarshal(byteData, &rawRelationalRecord)
 
+	var rawRelationalData []RawRelationalRecord
+	json.Unmarshal(util.ReadJSONFile(dataset), &rawRelationalData)
+
+	sort.Slice(rawRelationalData, func(i, j int) bool { return rawRelationalData[i].ID < rawRelationalData[j].ID })
+	rawRelationalRecord.ID = rawRelationalData[len(rawRelationalData)-1].ID + 1
+	relationalJSON, _ := json.Marshal(append(rawRelationalData, rawRelationalRecord))
+	WriteJSONData(dataset, relationalJSON)
+
 	lookupData := ParseLookup(util.ReadJSONFile(lookupDataset))
 	for _, lookupRecord := range lookupData {
 		if lookupRecord.ID == rawRelationalRecord.Related {
-			relationalData := ParseRelational(util.ReadJSONFile(dataset), lookupDataset)
-			sort.Slice(relationalData, func(i, j int) bool { return relationalData[i].ID < relationalData[j].ID })
-			relationalRecord := RelationalRecord{relationalData[len(relationalData)-1].ID + 1, rawRelationalRecord.Value, lookupRecord}
-			relationalData = append(relationalData, relationalRecord)
-			relationalJSON, _ := json.Marshal(relationalData)
-			WriteJSONData(dataset, relationalJSON)
-			return relationalRecord
+			return RelationalRecord{rawRelationalRecord.ID, rawRelationalRecord.Value, lookupRecord}
 		}
 	}
 	return RelationalRecord{999999, "Problem encountered, no data was Added", LookupRecord{10, "Related ID does not reference existing entry"}}
